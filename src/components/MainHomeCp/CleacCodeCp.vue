1708c459841d9cd886c175467ae280a3b69ad083
rahim einollahi <r.einollahi1@gmal.com>
2021-02-12 13:51:11 +0330

    add commit two

--- src/components/MainHomeCp/CleacCodeCp.vue
+++ src/components/MainHomeCp/CleacCodeCp.vue
@@ -1,6 +1,97 @@
 <template>
-    <div>
-        <p>clean code cp</p>
+    <div class="continer">
+            <div class="row  mt-1">
+                <div class="media border mt-1 p-3 mr-1 col-sm img-parent-hover">
+                    <img src="../../assets/photo/1.jpg" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
+                    <div class="media-body">
+                            <h4>book 1 <small><i> February 19, 2016</i></small></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>      
+                    </div>
+                </div>
+                <div class="media border mt-1 p-3 mr-1 col-sm img-parent-hover">
+                    <img src="../../assets/photo/2.jpg" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
+                    <div class="media-body">
+                            <h4>book2 <small><i> February 20, 2020</i></small></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>      
+                    </div>
+                </div>
+                <div class="media border mt-1 p-3 mr-1 col-sm img-parent-hover">
+                    <img src="../../assets/photo/3.jpg" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
+                    <div class="media-body">
+                            <h4>book3<small><i> February 2, 2010</i></small></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>      
+                    </div>
+                </div>
+            </div>
+            <div class="row  mt-1">
+                <div class="media border mt-1 p-3 mr-1 col-sm img-parent-hover">
+                    <img src="../../assets/photo/1.jpg" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
+                    <div class="media-body">
+                            <h4>book 1 <small><i> February 19, 2016</i></small></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>      
+                    </div>
+                </div>
+                <div class="media border mt-1 p-3 mr-1 col-sm img-parent-hover">
+                    <img src="../../assets/photo/4.jpg" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
+                    <div class="media-body">
+                            <h4>book2 <small><i> February 20, 2020</i></small></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>      
+                    </div>
+                </div>
+                <div class="media border mt-1 p-3 mr-1 col-sm img-parent-hover">
+                    <img src="../../assets/photo/5.jpg" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
+                    <div class="media-body">
+                            <h4>book3<small><i> February 2, 2010</i></small></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>      
+                    </div>
+                </div>
+            </div>
+            <div class="row  mt-1">
+                <div class="media border mt-1 p-3 mr-1 col-sm img-parent-hover">
+                    <img src="../../assets/photo/8.jpg" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
+                    <div class="media-body">
+                            <h4>book 1 <small><i> February 19, 2016</i></small></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>      
+                    </div>
+                </div>
+                <div class="media border mt-1 p-3 mr-1 col-sm img-parent-hover">
+                    <img src="../../assets/photo/9.jpg" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
+                    <div class="media-body">
+                            <h4>book2 <small><i> February 20, 2020</i></small></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>      
+                    </div>
+                </div>
+                <div class="media border mt-1 p-3 mr-1 col-sm img-parent-hover">
+                    <img src="../../assets/photo/3.jpg" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
+                    <div class="media-body">
+                            <h4>book3<small><i> February 2, 2010</i></small></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>      
+                    </div>
+                </div>
+            </div>
+            <div class="row  mt-1">
+                <div class="media border mt-1 p-3 mr-1 col-sm img-parent-hover">
+                    <img src="../../assets/photo/1.jpg" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
+                    <div class="media-body">
+                            <h4>book 1 <small><i> February 19, 2016</i></small></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>      
+                    </div>
+                </div>
+                <div class="media border mt-1 p-3 mr-1 col-sm img-parent-hover">
+                    <img src="../../assets/photo/8.jpg" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
+                    <div class="media-body">
+                            <h4>book2 <small><i> February 20, 2020</i></small></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>      
+                    </div>
+                </div>
+                <div class="media border mt-1 p-3 mr-1 col-sm img-parent-hover">
+                    <img src="../../assets/photo/3.jpg" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
+                    <div class="media-body">
+                            <h4>book3<small><i> February 2, 2010</i></small></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>      
+                    </div>
+                </div>
+            </div>
     </div>
 </template>
 
@@ -11,5 +102,23 @@
 </script>
 
 <style lang="scss" scoped>
+@import '../../assets/sass/base/_variable.scss';
+//clean code
+// $br-parent-color:rgb(255, 10, 104);
+// $br-img-color:rgb(236, 236, 200);
 
+.row-pad{
+    padding: 2px;
+}
+.img-parent-hover:hover{
+    opacity: 0.6;
+    border: 1px solid $br-parent-color;
+}
+img{
+    border: 1px solid black;
+}
+img:hover{
+    opacity: 0.5;
+    border: 1px solid $br-img-color;
+}
 </style>
--- src/components/MainHomeCp/HabbitAtomCp.vue
+++ src/components/MainHomeCp/HabbitAtomCp.vue
@@ -1,6 +1,22 @@
 <template>
-    <div>
-        <p>test ok</p>            
+    <div class="continer">
+        <!-- <p>test again</p> -->
+        <div id="home-habbit" class="row" v-for="item in habbitAtomItems" :key="item.id">
+            <h2 class="w-100">{{item.header}}</h2>
+            
+            <ul class="w-100 m-2 px-5 "  v-if="item.partText">
+                    <li class="col-sm-3 text-link" v-for="sub in item.partText" :key="sub"><a :href="sub.href">{{sub.text}}</a></li>
+            </ul>
+                
+             <div class="w-100" v-for="sub2 in item.partText" :key="sub2.text" >
+                 <hr class="mx-2">
+                 <h4 :id="sub2.id">{{sub2.text}}  <i class="fas fa-heart"> <a href="#home-habbit"> back</a></i></h4>
+                 <p class="px-3">{{sub2.content}}</p>
+            </div>   
+            
+        </div>   
+        <div class="w-100 text-center text-link "><a href="#home-habbit ">home</a></div>    
+       
     </div>
 </template>
 
@@ -8,11 +24,41 @@
     import {mapState} from 'vuex'
     export default {
         name:'HabbitAtomCp',
-        computed:mapState(['habbitBooks']),
+        computed:mapState(['habbitAtomItems']),
         
     }
 </script>
 
 <style lang="scss" scoped>
-
+@import '../../assets/sass/base/_variable.scss';
+@font-face {
+    font-family: 'habbit-font';
+    src: url('../../assets/fonts/Hanalei-Regular.ttf');
+}
+*{
+    font-family: 'habbit-font';
+}
+// $tbl-footer-shadow-color-orange:orange;
+// $tbl-footer-shadow-color-yellow:yellow;
+// $tbl-head-shadow-color-red:red;
+// $tbl-head-shadow-color-blue:blue;
+p{
+    text-align: justify;
+    text-indent: 50px;
+    line-height: 40px;
+}
+h2{
+    text-shadow: 5px 2px 10px $tbl-footer-shadow-color-orange,
+    2px 3px 15px  $tbl-footer-shadow-color-yellow;
+}
+h4{
+     text-shadow: 5px 2px 10px $tbl-head-shadow-color-red,
+    2px 3px 15px $tbl-footer-shadow-color-orange;
+}
+ul
+{
+    li{
+        list-style-type: circle;
+    }
+}
 </style>
